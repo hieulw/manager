bf967657b3a736b89b2a6daa5d72059a538af882
Murin Deventa <ladyga14@users.noreply.github.com>
2016-05-13 11:09:10 +0700

    reset to default
    user: admin
    pass: 12345

--- config.inc.php
+++ config.inc.php
@@ -1,1 +1,10 @@
-<?php if (!defined('ACCESS')) die('Not access'); else $configs = array('username' => 'ladyga14', 'password' => '50277f9c5b1fd539e3216b0f0eefecd7', 'page_list' => '0', 'page_file_edit' => '0', 'page_file_edit_line' => '0', 'page_database_list_rows' => '0'); ?>
+<?php
+if (!defined('ACCESS')) die('Not access');
+else $configs = array(
+    'username' => 'admin',
+    'password' => '827ccb0eea8a706c4c34a16891f84e7b',
+    'page_list' => '0',
+    'page_file_edit' => '0',
+    'page_file_edit_line' => '0',
+    'page_database_list_rows' => '0'
+);
