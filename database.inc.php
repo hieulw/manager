031c9dba3f90ebfb145a6dcb6425208d703a9546
Murin Deventa <ladyga14@users.noreply.github.com>
2016-05-13 11:05:23 +0700

    reset default

--- database.inc.php
+++ database.inc.php
@@ -1,1 +1,9 @@
-<?php if (!defined('ACCESS')) die('Not access'); else $databases = array('db_host' => 'localhost', 'db_username' => 'adminwMnxUvD', 'db_password' => 'CWlkf1lh5wNA', 'db_name' => 'dev', 'is_auto' => false); ?>
+<?php
+if (!defined('ACCESS')) die('Not access');
+else $databases = array(
+    'db_host' => 'localhost',
+    'db_username' => '',
+    'db_password' => '',
+    'db_name' => '',
+    'is_auto' => false
+);
