c889ac8e986b1a60b765b2fcabeb66eb14bf6630
Murin Deventa <ladyga14@users.noreply.github.com>
2016-06-02 00:48:27 +0700

    fix password

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 if (!defined('ACCESS')) die('Not access');
 else $configs = array(
     'username' => 'admin',
-    'password' => '827ccb0eea8a706c4c34a16891f84e7b',
+    'password' => '1f32aa4c9a1d2ea010adcf2348166a04',
     'page_list' => '0',
     'page_file_edit' => '0',
     'page_file_edit_line' => '0',
