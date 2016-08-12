107ceff420e2419c263c6180bd64f42f724c916a
Murin Deventa <ladyga14@users.noreply.github.com>
2016-08-12 22:09:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<title>Cài đặt thành công</title>
+
+<h1>Chúc mừng bạn đã cài đặt thành công Heroku App!</h1>
+
+Hãy đăng nhập vào <a href="/manager">manager</a> để quản lí file hoặc up code. Xem thêm về thông tin gói php của Heroku <a href="/info.php">tại đây</a>
+
+Sau khi đăng nhập thành công hãy xóa các file 
+<ul>
+ <li><b>index.php</b></li>
+ <li><b>info.php</b></li>
+ <li><b>README.md</b></li>
+</ul>
+Nếu bạn không cần dùng composer hãy xóa <b>composer.json</b> và mục <b>vendor</b>
+
+<h3><font color="red">Lưu ý!</font></h3>
+Không được xóa các tệp khác ngoài các tệp nêu trên, như <b>.heroku</b>, <b>.composer</b> v.vv
